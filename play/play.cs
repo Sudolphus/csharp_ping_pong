970ef96bcda8c8c0069bef30dc07520ee9603eae
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-13 11:24:01 -0700

    add method for numbers divisible by 15
    Co-Authored-By: Spencer Moody <spencer.moody@outlook.com>

---
+++ play/play.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace PingPong.Play
+{
+  public class PingPong
+  {
+    public static string PlayPingPong(int maxNumber)
+    {
+      List<string> outputList = new List<string>(0);
+      for (int i = 1; i <= maxNumber; i++)
+      {
+        
+      }
+    }
+  }
+}
